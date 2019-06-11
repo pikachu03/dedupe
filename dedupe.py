4e8c67b2f7b918c6661c75777f834348b0df6ea2
pikachu03 <37090427+pikachu03@users.noreply.github.com>
2019-06-10 21:38:48 -0400

    bug fixes

--- dedupe.py
+++ dedupe.py
@@ -2,6 +2,7 @@ import os
 import hashlib
 import operator
 
+
 def md5(fname):
     hash_md5 = hashlib.md5()
     with open(fname) as f:
@@ -50,7 +51,7 @@ def dedupe(p1, p2=''):
 
     def b():
         print("Calculating hashes...\n")
-        temp = sub_am
+        temp = sub_a
         for i in range(len(sub_a)):                          
             for j in range(len(sub_a[i])):
                 if not len(sub_a[i]) == 1:
